51b1f9d03a67f07f1577281da391801af7541fb6
Scott B5 <busingescott@gmail.com>
2016-11-04 19:39:26 +0300

    my first passed test

--- quotes/tests.py
+++ quotes/tests.py
@@ -1,2 +1,22 @@
-from django.test import TestCase
+from selenium import webdriver
+import unittest
 
+
+class FirstTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(5)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_to_check_django_page(self):
+        # User opens browser
+        self.browser.get('http://localhost:8000')
+
+        body = self.browser.find_elements_by_tag_name('body')
+        assert 'Django' in self.browser.title
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
