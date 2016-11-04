0d9667eeb60fedaf75e9952f1b767e588693de6a
Scott B5 <busingescott@gmail.com>
2016-11-04 21:40:23 +0300

    refactored my tests code

--- quotes/tests.py
+++ quotes/tests.py
@@ -1,4 +1,3 @@
-from dominate.tags import body
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
@@ -16,14 +15,31 @@ class FirstTest(unittest.TestCase):
         # User opens browser
         self.browser.get('http://localhost:8000')
 
-        body = self.browser.find_elements_by_tag_name('body')
-        assert 'Django' in self.browser.title
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Django', body.text)
 
-    def test_for_admin(self):
         # User opens admin page
         self.browser.get('http://localhost:8000/admin/')
+
         body = self.browser.find_element_by_tag_name('body')
         self.assertIn('Django administration', body.text)
+
+        # User opens admin page
+        self.browser.get('http://localhost:8000/admin/')
+
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Django administration', body.text)
+
+        # users types in username and passwords and presses enter
+        username_field = self.browser.find_element_by_name('username')
+        username_field.send_keys('quotes')
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('quotes')
+        password_field.send_keys(Keys.RETURN)
+
+        # login credentials are correct, and the user is redirected to the main admin page
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Site administration', body.text)
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
