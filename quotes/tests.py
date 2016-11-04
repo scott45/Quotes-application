f68e184825ca2c3db1cdf7156f4907bf87956ff3
Scott B5 <busingescott@gmail.com>
2016-11-04 19:58:10 +0300

    second test for admin page

--- quotes/tests.py
+++ quotes/tests.py
@@ -1,4 +1,6 @@
+from dominate.tags import body
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -17,6 +19,11 @@ class FirstTest(unittest.TestCase):
         body = self.browser.find_elements_by_tag_name('body')
         assert 'Django' in self.browser.title
 
+    def test_for_admin(self):
+        # User opens admin page
+        self.browser.get('http://localhost:8000/admin/')
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Django administration', body.text)
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
