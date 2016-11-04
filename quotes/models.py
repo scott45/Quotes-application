81070f4363186692c61aa53a06a1a8390ae14dc6
Scott B5 <busingescott@gmail.com>
2016-11-04 22:29:01 +0300

    my model tests commit

--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# my quotes model
+
+class Quote(models.Model):
+    title = models.CharField("Quote", max_length=100)
+    submitter = models.ForeignKey(User)
+    submitted_on = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
+
+
--- quotes/tests.py
+++ quotes/tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+from .models import Quote
 
 
 class FirstTest(unittest.TestCase):
@@ -12,25 +13,30 @@ class FirstTest(unittest.TestCase):
         self.browser.quit()
 
     def test_to_check_django_page(self):
+
         # User opens browser
+
         self.browser.get('http://localhost:8000')
 
         body = self.browser.find_element_by_tag_name('body')
         self.assertIn('Django', body.text)
 
         # User opens admin page
+
         self.browser.get('http://localhost:8000/admin/')
 
         body = self.browser.find_element_by_tag_name('body')
         self.assertIn('Django administration', body.text)
 
         # User opens admin page
+
         self.browser.get('http://localhost:8000/admin/')
 
         body = self.browser.find_element_by_tag_name('body')
         self.assertIn('Django administration', body.text)
 
         # users types in username and passwords and presses enter
+
         username_field = self.browser.find_element_by_name('username')
         username_field.send_keys('quotes')
         password_field = self.browser.find_element_by_name('password')
@@ -38,8 +44,29 @@ class FirstTest(unittest.TestCase):
         password_field.send_keys(Keys.RETURN)
 
         # login credentials are correct, and the user is redirected to the main admin page
+
         body = self.browser.find_element_by_tag_name('body')
         self.assertIn('Site administration', body.text)
+
+        # the user is redirected to the main admin page to ensure database models are present
+
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Quotes', body.text)
+
+        # the user is redirected to the main admin page to ensure database models are present
+
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Votes', body.text)
+
+        # to test string representation for models
+
+        quote = Quote(title="Quote title")
+        self.assertEqual(str(quote), quote.title)
+
+        # to test for verbose plural name
+
+        self.assertEqual(str(Quote._meta.verbose_name_plural), "quotes")
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
