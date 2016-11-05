a02f9075136e15f063d88b00923d468aff1f40a2
Scott B5 <busingescott@gmail.com>
2016-11-05 21:51:05 +0300

    test for index page and it's code

--- Quote/urls.py
+++ Quote/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from quotes import views
 
-urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-]
+urlpatterns = patterns('',
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^index/$', views.index, name='index')
+                       )
--- quotes/tests.py
+++ quotes/tests.py
@@ -3,3 +3,21 @@
 # while Unit tests are the means to help you reach that end. This will all make sense soon.
 
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+
+from .views import index
+
+
+class IndexPageTest(TestCase):
+
+    def test_root_url_resolves_to_index_page_view(self):
+        found = resolve('/index/')
+        self.assertEqual(found.func, index)
+
+    def test_index_returns_right_html(self):
+        request = HttpRequest()
+        response = index(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>quotes</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
