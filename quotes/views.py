a302249adbf6487a9f2b689316ecbbc4f8b62e0c
Scott B5 <busingescott@gmail.com>
2016-11-09 17:56:29 +0300

    changed views to use generic views

--- quotes/views.py
+++ quotes/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
+from django.views import generic
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from .models import Quote
+from django.core.urlresolvers import reverse_lazy
 
-def index(request):
-    return render(request, 'index.html')
+class IndexView(generic.ListView):
+    template_name = "index.html"
+    context_object_name = "quotes"
+
+    def get_queryset(self):
+        return Quote.objects.all()
+
+
+class DetailView(generic.DetailView):
+    model = Quote
+    template_name = "detail.html"
+
+
+class DepartmentCreate(CreateView):
+    model = Quote
+    fields = ['Name', 'Leader', 'Department_logo', 'is_favorite']
+    template_name = 'department_form.html'
+
+
+class DepartmentUpdate(CreateView):
+    model = Quote
+    fields = ['Name', 'Leader', 'Department_logo', 'is_favorite']
+
+
+class DepartmentDelete(DeleteView):
+    model = Quote
+    success_url = reverse_lazy('index')
+
 
