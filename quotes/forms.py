6eb2a81515b4087dee0689ea39aa6eb0848d3ece
Scott B5 <busingescott@gmail.com>
2016-11-27 18:56:20 +0300

    included django forms

--- quotes/forms.py
+++ quotes/forms.py
@@ -1,3 +1,17 @@
-__author__ = 'Scott Businge'
+from django import forms
+from .models import Quote
+from django.contrib.auth.models import User
 
 
+class StoreForm(forms.ModelForm):
+    class Meta:
+        model = Quote
+        fields = ('title', 'description')
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
